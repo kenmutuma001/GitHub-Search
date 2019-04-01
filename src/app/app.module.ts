ab6f35c943455d2cf12e43850393485b8866699c
kennedymutuma <santa@northpole.com>
2019-04-01 12:36:44 +0300

    add the input field

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SearchComponent,
+    AboutUsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
